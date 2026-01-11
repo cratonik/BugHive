98b14b5ff25a6283c48f119b808957d52b83d42b
Kaushal Saple <saplekaushal@gmail.com>
2026-01-11 18:00:38 +0530

    Changed port number from 8080 to 8083

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://localhost:8083/api',
     timeout: 5000,
     headers:{
         'Content-Type': 'application/json',
