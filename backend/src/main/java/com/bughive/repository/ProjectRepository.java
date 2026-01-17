69a44297028456d52a0a796ab53f45d6ad950061
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2026-01-17 19:50:37 +0530

    Created setup of User APIs.

--- backend/src/main/java/com/bughive/repository/ProjectRepository.java
+++ backend/src/main/java/com/bughive/repository/ProjectRepository.java
@@ -3,6 +3,7 @@ package com.bughive.repository;
 import com.bughive.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,13 +12,14 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findAllWithCreator();
 
     @Query("SELECT p FROM Project p JOIN FETCH p.createdBy WHERE p.id = :id")
-    Project findByIdWithCreator(Long id);
+    Project findByIdWithCreator(@Param("id") Long id);
 
     @Query("""
                 SELECT p, COUNT(i)
                 FROM Project p
+                JOIN FETCH p.createdBy
                 LEFT JOIN Issue i ON i.project.id = p.id
-                GROUP BY p
+                GROUP BY p, p.createdBy
             """)
     List<Object[]> findAllProjectsWithIssueCounts();
 
---
+++ backend/src/main/java/com/bughive/service/UserService.java
@@ -0,0 +1,50 @@
+package com.bughive.service;
+
+import com.bughive.dto.UserRequest;
+import com.bughive.dto.UserResponse;
+import com.bughive.entity.User;
+import com.bughive.mapper.UserMapper;
+import com.bughive.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public List<UserResponse> findAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto).toList();
+    }
+
+    public Optional<UserResponse> findByUserId(Long userId) {
+        return userRepository.findById(userId)
+                .map(userMapper::toDto);
+    }
+
+    public UserResponse createUser(UserRequest userRequest) {
+        if(userRequest.getEmail() == null ){
+            throw new IllegalArgumentException("Email is required");
+        }
+        if(userRequest.getPassword() == null ){
+            throw new IllegalArgumentException("Password is required");
+        }
+        if(userRequest.getPassword().length() < 6){
+            throw new IllegalArgumentException("Password length is required");
+        }
+        userRepository.findByEmail(userRequest.getEmail())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("Email already in use");
+                });
+        User user = userMapper.toEntity(userRequest);
+        User saved = userRepository.save(user);
+        return userMapper.toDto(saved);
+    }
+}
