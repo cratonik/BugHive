6b8f27d411ac69b49ca17b3210b0a758fad36275
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2026-01-19 20:48:18 +0530

    Removed unnessary code

--- backend/src/main/java/com/bughive/service/UserService.java
+++ backend/src/main/java/com/bughive/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
 import java.util.Optional;
 
